7eb895ba1ff652a724b2168ad65e350358903811
liangchenSilk <32463010+liangchenSilk@users.noreply.github.com>
2019-02-26 23:08:13 -0800

    init

---
+++ db/db_info.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 17 16:25:03 2019
+
+@author: Silk
+"""
+
+config = {
+        'host' : '127.0.0.1',
+        'user' : 'root',
+        'password' : 'root',
+        'port' : 3308,
+        'database' : 'austin'
+}
